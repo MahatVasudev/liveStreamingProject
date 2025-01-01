07d1797f0f3f984f92566d77613de87711562836
MahatVasudev <mahatvasudev9@gmail.com>
2025-01-01 17:45:51 +0530

    Starting Server

---
+++ server/cmd/api/server.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"flag"
+	"os"
+)
+
+var (
+	addr = flag.String("addr", ":", os.Getenv("PORT"))
+	cert = flag.String("cert", "", "")
+	key  = flag.String("key", "", "")
+)
+
+func Run() error {
+	flag.Parse()
+
+	if *addr == ":" {
+		*addr = ":8080"
+	}
+
+	return nil
+}
