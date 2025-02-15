23861cea00cdac8b5e73d5e9fe4bae8502922857
MahatVasudev <mahatvasudev9@gmail.com>
2025-02-15 20:34:15 +0530

    Added Start Logic

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+
+	"github.com/MahatVasudev/liveStreamingProject/server/cmd/api"
+)
+
+func main() {
+	addr := ":5000"
+	api1 := api.NewApiServer(addr, nil, nil)
+
+	log.Printf("Server Starting at Address %s....", addr)
+
+	if err := api1.Run(); err != nil {
+		log.Panic("Server Error!!!")
+	}
+
+	log.Println("Server is offline....")
+}
